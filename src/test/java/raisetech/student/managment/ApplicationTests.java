b5ea86ef0f7c63316dc39665880ab47a5de91113
Takumi Hamamoto <shinnsekai5@gmail.com>
2026-03-15 11:10:53 +0900

    firstcommit

---
+++ src/test/java/raisetech/student/managment/ApplicationTests.java
@@ -0,0 +1,13 @@
+package raisetech.student.managment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
